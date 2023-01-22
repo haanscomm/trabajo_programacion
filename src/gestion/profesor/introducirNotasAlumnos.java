19ad2d04f1456f8800f3b42663d4e049b0e28b70
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2023-01-22 12:05:23 +0100

    cambios de ángela en introducir notas

--- src/gestion/profesor/introducirNotasAlumnos.java
+++ src/gestion/profesor/introducirNotasAlumnos.java
@@ -1,7 +1,8 @@
 package gestion.profesor;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -10,16 +11,31 @@ import ficheros.*;
 public class introducirNotasAlumnos {
 
 
-	public static void introducirNotas(String asignatura) throws FileNotFoundException {
+	public static void introducirNotas(String asignatura) throws IOException {
 		
-		Scanner leer = new Scanner (System.in);
+		Scanner l = new Scanner (System.in);
 		File fichero = new File("notas_alumnos.txt");
 		PrintWriter escribir = new PrintWriter(fichero);
-		System.out.println();
 		
 		
 		//creamos el fichero dni_alumnos
 		DNI_alumnos.dni_alumnos();
+		
+		for(int i = 1; i <= 15; i++) {
+		System.out.println("Introduce la nota del alumno " + i + ": ");
+		double nota = l.nextDouble();
+		if(nota > 0 && nota < 10) {
+		escribir.print("La nota del alumno "+ i + " es: " +nota + "\n");
+		} else {
+			System.out.println("La nota tiene que ser entre 0 y 10");
+			System.out.println("Introduce la nota del alumno " + i + ": ");
+			nota = l.nextDouble();
+		 }
+		}
+		Scanner leer = new Scanner(new FileReader(fichero));
+		leer.close();
+		escribir.close();
+		
 		//creamos un array de la asignatura 
 		
 		//Exploramos fichero dni_alumnos 
@@ -29,6 +45,7 @@ public class introducirNotasAlumnos {
 	}
 	
 	public static void calcularNotaMasAlta() {
+		//HOLA BUENAS
 		
 	}
 	
