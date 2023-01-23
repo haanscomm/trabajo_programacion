63f8d53894ad8f10c726ab93ad13f38024ee5406
haanscomm <94965519+haanscomm@users.noreply.github.com>
2023-01-23 02:09:02 +0100

    funciones

--- src/gestion/profesor/introducirNotasAlumnos.java
+++ src/gestion/profesor/introducirNotasAlumnos.java
@@ -9,29 +9,33 @@ import java.util.Scanner;
 import ficheros.*;
 
 public class introducirNotasAlumnos {
-	
+
 	public static void main(String asignatura, int opcion) throws IOException {
-		
+
 		File fichero1 = new File("alumnos.txt");
 		int[] notas = new int[contarLineasFichero(fichero1)];
-		
-		if(opcion == 1) {
-			introducirNotas(asignatura, notas);
-		}else if (opcion == 2) {
-			calcularMedia(asignatura, notas);
-		}else if (opcion == 3) {
+
+		notas = introducirNotas(asignatura, notas);
+
+		if (opcion == 2) {
+
+			int media = calcularMedia(asignatura, notas);
+			System.out.println("La nota media es : " + media);
+		} else if (opcion == 3) {
+
 			calcularModa(asignatura, notas);
-		}else if (opcion == 4) {
-			calcularNotaMasAlta(asignatura, notas);
-		}else {
-			calcularNotaMasBaja(asignatura, notas);
+		} else if (opcion == 4) {
+
+			int max = calcularNotaMasAlta(asignatura, notas);
+			System.out.println("La nota maxima es : " + max);
+		} else {
+			int min = calcularNotaMasBaja(asignatura, notas);
+			System.out.println("La nota minima es : " + min);
 		}
-		
-		
-		
+
 	}
 
-	public static void introducirNotas(String asignatura, int [] notas) throws IOException {
+	public static int[] introducirNotas(String asignatura, int[] notas) throws IOException {
 
 		Scanner l = new Scanner(System.in);
 		String asignatura2 = asignatura + ".txt";
@@ -41,46 +45,29 @@ public class introducirNotasAlumnos {
 
 		salida = new PrintWriter(new FileWriter(fichero));
 
-		// creamos el fichero dni_alumnos
 		DNI_alumnos.dni_alumnos();
 
-		/*
-		 * for(int i = 1; i <= 15; i++) {
-		 * System.out.println("Introduce la nota del alumno " + i + ": "); double nota =
-		 * l.nextDouble(); if(nota > 0 && nota < 10) {
-		 * escribir.print("La nota del alumno "+ i + " es: " +nota + "\n"); } else {
-		 * System.out.println("La nota tiene que ser entre 0 y 10");
-		 * System.out.println("Introduce la nota del alumno " + i + ": "); nota =
-		 * l.nextDouble(); } } Scanner leer = new Scanner(new FileReader(fichero));
-		 * leer.close(); escribir.close();
-		 */
-
-		// creamos un array de la asignatura
-
-		// Exploramos fichero dni_alumnos y lo introducimos en uno nuevo
 		Scanner lectura_fichero = new Scanner(fichero1);
-		
-		
+
 		int i = 0;
 		while (lectura_fichero.hasNextLine()) {
-			
 
 			String datos = lectura_fichero.nextLine();
-			salida.print("Alumno con DNI:");
-			salida.println(datos);
+			salida.print("Alumno DNI:");
+			salida.print(datos + " ");
 
 			System.out.println("Introduzca la nota para el alumno " + datos + ": ");
 			double nota1 = l.nextDouble();
-			
-			if (nota1 >= 0 && nota1 <= 10) {
-	
-			notas[i] = (int) nota1;
-			salida.println("nota:" + notas[i]);
 
-			i++;
-			
+			if (nota1 >= 0 && nota1 <= 10) {
+
+				notas[i] = (int) nota1;
+				salida.println("nota:" + notas[i]);
+
+				i++;
+
 			} else {
-				
+
 				System.out.println("La nota tiene que ser entre 0 y 10");
 				System.out.println("Introduzca la nota para el alumno " + datos + ": ");
 				nota1 = l.nextDouble();
@@ -88,20 +75,8 @@ public class introducirNotasAlumnos {
 				salida.println("nota:" + notas[i]);
 
 				i++;
-				
-			}
 
-		/*for(int i = 1; i <= 15; i++) {
-				System.out.println("Introduce la nota del alumno " + i + ": ");
-				double nota = l.nextDouble();
-				if(nota > 0 && nota < 10) {
-				salida.print("La nota del alumno "+ i + " es: " +nota + "\n");
-				} else {
-					System.out.println("La nota tiene que ser entre 0 y 10");
-					System.out.println("Introduce la nota del alumno " + i + ": ");
-					nota = l.nextDouble();
-				 }
-				}*/
+			}
 
 		}
 
@@ -110,37 +85,51 @@ public class introducirNotasAlumnos {
 
 		l.close();
 
+		return notas;
 
 	}
 
-	public static void calcularNotaMasAlta(String asignatura, int [] notas) throws IOException {
+	public static int calcularNotaMasAlta(String asignatura, int[] notas) throws IOException {
+		int max = 0;
 
-		String asignaturaFichero = asignatura + ".txt";
-		File archivo = new File(asignaturaFichero);
+		for (int i = 0; i < notas.length; i++) {
+			if (max < notas[i]) {
+				max = notas[i];
+			}
 
-		if (!archivo.exists()) {
-			
-			
-
-			
-		} else {
-			System.out.println("Por favor, introduzca las notas para calcular la nota mas alta.");
-
-			introducirNotas(asignatura, notas);
-			System.out.println("Hola");
 		}
 
-	}
-
-	public static void calcularNotaMasBaja(String asignatura, int [] notas) {
+		return max;
 
 	}
 
-	public static void calcularMedia(String asignatura, int [] notas) {
+	public static int calcularNotaMasBaja(String asignatura, int[] notas) {
+
+		int min = 0;
+
+		for (int i = 0; i < notas.length; i++) {
+			if (min < notas[i]) {
+				min = notas[i];
+			}
+
+		}
+
+		return min;
+	}
+
+	public static int calcularMedia(String asignatura, int[] notas) {
+
+		int media = 0;
+		int sumanotas = 0;
+		for (int i = 0; i < notas.length; i++) {
+			sumanotas += notas[i];
+		}
+		media = sumanotas / notas.length;
+		return media;
 
 	}
 
-	public static void calcularModa(String asignatura, int [] notas) {
+	public static void calcularModa(String asignatura, int[] notas) {
 
 	}
 
